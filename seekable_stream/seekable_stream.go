c9c869e4da5d9a53cbbcaea4fdf4aa8b20a13469
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2016-09-07 11:23:39 -0400

    Added new struct SeekableStream

---
+++ seekable_stream/seekable_stream.go
@@ -0,0 +1,38 @@
+package seekable_stream
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"io"
+)
+
+type SeekableStream struct {
+	bytes []byte
+	*bytes.Reader
+}
+
+func (ss *SeekableStream) WrapBytes(b []byte) {
+	ss.bytes = b
+	ss.Reader = bytes.NewReader(ss.bytes)
+}
+
+func (ss *SeekableStream) WrapReader(reader io.Reader) (err error) {
+	buf := new(bytes.Buffer)
+	if _, err = io.Copy(buf, reader); err != nil {
+		err = errors.New(fmt.Sprintf("Failed to wrap reader: %v", err))
+		return
+	}
+	ss.bytes = make([]byte, buf.Len())
+	copy(ss.bytes, buf.Bytes())
+	ss.Reader = bytes.NewReader(ss.bytes)
+	return
+}
+
+func (ss *SeekableStream) Rewind() {
+	ss.Reader = bytes.NewReader(ss.bytes)
+}
+
+func (ss *SeekableStream) String() string {
+	return string(ss.bytes)
+}
---
+++ seekable_stream/seekable_stream_test.go
@@ -0,0 +1,64 @@
+package seekable_stream
+
+import (
+	"bytes"
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type DummyReader int
+
+func (dr *DummyReader) Read(b []byte) (n int, err error) {
+	err = errors.New("Throwing error: Expected")
+	return
+}
+
+func TestWrapBytes(t *testing.T) {
+	t.Parallel()
+
+	assert := assert.New(t)
+
+	// Try to wrap around []byte
+	ss := new(SeekableStream)
+	buf := new(bytes.Buffer)
+	buf.WriteString("Hello, World!")
+	ss.WrapBytes(buf.Bytes())
+	output := ss.String()
+	assert.Equal(buf.String(), output, "Strings don't match")
+	// Do a rewind
+	ss.Rewind()
+	output = ss.String()
+	assert.Equal(buf.String(), output, "Strings don't match")
+
+}
+
+func TestWrapBuffer(t *testing.T) {
+	t.Parallel()
+
+	assert := assert.New(t)
+
+	// Try to wrap around bytes.Buffer
+	buf := new(bytes.Buffer)
+	str := "Hello, World!-1"
+	buf.WriteString(str)
+	ss := new(SeekableStream)
+	ss.WrapReader(buf)
+	output := ss.String()
+	assert.Equal(str, output, "Strings don't match")
+	ss.Rewind()
+	output = ss.String()
+	assert.Equal(str, output, "Strings don't match")
+}
+
+func TestError(t *testing.T) {
+	t.Parallel()
+
+	assert := assert.New(t)
+
+	ss := new(SeekableStream)
+	dr := new(DummyReader)
+	err := ss.WrapReader(dr)
+	assert.NotNil(err, "Expected error when using a reader that throws error while reading")
+}
